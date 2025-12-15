5931ef467c6349a86a56143a107b1b06d00dd5b7
Gavin Bains <gavin@thellmdatacompany.com>
2025-12-14 23:31:12 -0800

    build(release): v1.2.5 (#14)

--- src/rubric/__init__.py
+++ src/rubric/__init__.py
@@ -1,7 +1,7 @@
 from rubric.rubric import Rubric
 from rubric.types import Criterion, CriterionReport, EvaluationReport
 
-__version__ = "1.2.4"
+__version__ = "1.2.5"
 __all__ = ["Criterion", "CriterionReport", "EvaluationReport", "Rubric"]
 __name__ = "rubric"
 __author__ = "The LLM Data Company"
